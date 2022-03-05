2f62340a1619e4a68af4289307646565e61d2b76
Mohamad Farid Saleh <githubfarid1@gmail.com>
2022-03-05 22:44:34 +0900

    update oc.sh

--- opencart-exist-2302.sh
+++ opencart-exist-2302.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "BEGIN ..."
 HOME_DIR=/var/www/html/public
 ROOT_DIR=/var/www/html
 
 docker exec $(docker-compose ps -q php) chmod 777 $HOME_DIR/image/cache/ $HOME_DIR/system/storage/modification/ $HOME_DIR/system/storage/logs/ $HOME_DIR/system/storage/cache/ -R
+echo "DONE ..."
